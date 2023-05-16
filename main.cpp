0f52603b148a4367d8ecabc4b6758306ae754586
Nastya F <fedorina.nastya@mail.ru>
2023-05-16 15:54:39 +0700

    Начало работы
    Работает корректно, но необходимо русифицировать

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <QCoreApplication>
+#include "win.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
+    QApplication a(argc, argv);
+    Win w;
+    w.show();
     return a.exec();
 }
--- win.h
+++ win.h
@@ -1,4 +1,40 @@
 #ifndef WIN_H
 #define WIN_H
+#include <QtGui>
+#include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
+
+class Win:public QWidget
+{
+    Q_OBJECT
+protected:
+    QTextCodec *codec;
+    QFrame *frame;
+    QLabel *inputLabel;
+    QLineEdit *inputEdit;
+    QLabel *outputLabel;
+    QLineEdit *outputEdit;
+    QPushButton *nextButton;
+    QPushButton *exitButton;
+public:
+    Win(QWidget *parent = 0);
+public slots:
+    void begin();
+    void calc();
+};
+
+class StrValidator:public QValidator
+{
+public:
+    StrValidator(QObject *parent):QValidator(parent){}
+    virtual State validate(QString &str,int &pos) const
+    {
+        return Acceptable;
+    }
+};
 
 #endif // WIN_H
